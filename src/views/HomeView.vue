be0ee1d72d9acf93ca1b824680f541eadc56dcfe
unknown <rehaburakacar1907@gmail.com>
2023-01-13 13:29:03 +0300

    structured clone problem solved

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class = "gridHome">
-        reha burak acar
+    <div class="home">
+      <GridComp :table="table"/>
     </div>
-</template>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  
+  // @ is an alias to /src
+  import { factset as stach } from '@/stach-sdk'
+  import GridComp from '@/components/Grid/GridComponent.vue' // NOSONAR
+  
+  type ITable = stach.protobuf.stach.v2.RowOrganizedPackage.ITable
+  
+  const table = ref<ITable | null>(null)
+  
+  fetch('stach-row-organized-package.json')
+    .then(stachDataRaw => {
+      return stachDataRaw.json()
+    })
+    .then(stachRowOrganizedPackage => {
+      table.value = stachRowOrganizedPackage.tables.main as unknown as ITable
+    })
+  </script>
+  
